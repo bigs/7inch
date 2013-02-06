cb6d4a5128bbaf4be4132945590b256c95643d8d
Cole Brown <bigswim@gmail.com>
2013-02-06 18:09:28 -0500

    Update boss probabilities

--- src/Modules/Boss.hs
+++ src/Modules/Boss.hs
@@ -9,14 +9,14 @@ import System.IO
 import System.Random
 
 isBoss :: IrcMsg -> Bool
-isBoss (PubMsg PRIVMSG _ _ msg) = msg =~ "^[a-z]+$"
+isBoss (PubMsg PRIVMSG _ _ msg) = msg =~ "^[a-zA-Z-_.]+$"
 isBoss _ = False
 
 bossHandler :: Handle -> IrcMsg -> SocketHandler -> IO ()
 bossHandler h (PubMsg _ _ c msg) cb = do
   let response = msg ++ ", boss"
   r <- getStdRandom (randomR (0 :: Integer, 1000 :: Integer))
-  if r < 10
+  if r < 100
     then sendCmd h PRIVMSG [channelToString c, response] >> cb
     else cb
 
